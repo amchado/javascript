70f8e0a1634e5779c3695e29bbf336c55cf9365d
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2023-02-08 17:56:52 -0300

    criação do ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [ 8, 5 , 3 , 9]
+num[3]= 1
+num[4] = 2
+num.push(7)
+num.sort()          //num.sort()
+
+
+console.log(`Minha variavel é ${num}`)
+console.log(`O vetor tem ${num.length} posições`) //num.length
